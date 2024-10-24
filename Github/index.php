83830e08f0a6d6e877c6053cb381dafc10790233
Meteofloralies <embrun72@gmail.com>
2024-10-24 09:40:19 +0200

    Update index.php

--- Github/index.php
+++ Github/index.php
@@ -25,19 +25,19 @@
 
 
   
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Index - Météo Floralies</title>
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Index - MÃ©tÃ©o Floralies</title>
   
 
   
   
   <meta content="Guy" name="author">
 
-  <base href="http://meteofloralies.free.fr/">
+  <base href="https://meteofloralies.github.io">
   
   <script type="text/javascript">
 function affichePage()
 {
-// récupération des données : annee / mois / pages
+// rÃ©cupÃ©ration des donnÃ©es : annee / mois / pages
 var annee = document.getElementById('annee').value;
 var mois = document.getElementById('mois').value
 var pages = document.getElementById('pages').value
@@ -66,7 +66,7 @@ a:visited { color:#dcd296; }
   </style>
   
   <script>
-// récupération des données : annee / mois / pages
+// rÃ©cupÃ©ration des donnÃ©es : annee / mois / pages
 var annee = document.getElementById('annee').value;
 var mois = document.getElementById('mois').value
 var pages = document.getElementById('pages').value
@@ -96,15 +96,15 @@ background-color:rgb(0, 51, 153);
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" _base_href="http://meteofloralies.free.fr/">
 
   
-  <style _base_href="http://meteofloralies.free.fr/">
+  <style _base_href="https://meteofloralies.github.io">
 #widget_9d5499367e786dbed2626a433dae589b {margin: auto;}
   </style>
   
   <link href="cligne2.css" rel="stylesheet" type="text/css"></head><body style="color: white; background-color: rgb(0, 51, 153);" alink="#000088" link="white" vlink="#ff0000">
-<div style="text-align: center;"><big><big style="font-weight: bold; color: rgb(255, 255, 51);"><span style="color: red;">M</span><span style="color: rgb(102, 255, 255);">é</span>t<span style="color: rgb(255, 153, 255);">é</span><span style="color: red;">o</span>f<span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(51, 255, 51);">o</span><span style="color: red;">r<span style="color: white;">a</span></span><span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(255, 153, 255);">i</span><span style="color: white;">e</span><span style="color: red;">s</span></big></big></div>
+<div style="text-align: center;"><big><big style="font-weight: bold; color: rgb(255, 255, 51);"><span style="color: red;">M</span><span style="color: rgb(102, 255, 255);">Ã©</span>t<span style="color: rgb(255, 153, 255);">Ã©</span><span style="color: red;">o</span>f<span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(51, 255, 51);">o</span><span style="color: red;">r<span style="color: white;">a</span></span><span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(255, 153, 255);">i</span><span style="color: white;">e</span><span style="color: red;">s</span></big></big></div>
 
 <p class="cligne2" style="text-align: center;"><span style="color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La
-mise à jour se fera le plus souvent possible<small><br>
+mise Ã  jour se fera le plus souvent possible<small><br>
 </small></span></p>
 
 
@@ -123,16 +123,16 @@ Liens vers...<br>
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
-Embarcadère&nbsp; &nbsp;&nbsp;
+EmbarcadÃ¨re&nbsp; &nbsp;&nbsp;
 En
-direct&nbsp; &nbsp;<small><small>&nbsp;</small></small>&nbsp; &nbsp; Mobile &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; Mairie&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La Terrière
+direct&nbsp; &nbsp;<small><small>&nbsp;</small></small>&nbsp; &nbsp; Mobile &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; Mairie&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La TerriÃ¨re
  </div>
 
-<div style="text-align: center;"><a href="https://pv.viewsurf.com/2094/La-Tranche-sur-mer-Embarcadere?i=NzYxMDp1bmRlZmluZWQ" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 36px; height: 37px;" alt="Embarcadère" src="/Icones/Webcam.png" _base_href="http://meteofloralies.free.fr/"></a>&nbsp;&nbsp;&nbsp;
+<div style="text-align: center;"><a href="https://pv.viewsurf.com/2094/La-Tranche-sur-mer-Embarcadere?i=NzYxMDp1bmRlZmluZWQ" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 36px; height: 37px;" alt="EmbarcadÃ¨re" src="/Icones/Webcam.png" _base_href="http://meteofloralies.free.fr/"></a>&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;  <a href="https://www.wunderground.com/dashboard/pws/ILATRA19/table/2020-00-8/2020-00-8/daily" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 50px; height: 36px;" alt="En direct" src="Icones/HP2550.jpg" _base_href="http://meteofloralies.free.fr/"></a> &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<a href="/2024/Octobre/Mobile.html"><img style="border: 0px solid ; width: 34px; height: 61px;" alt="Mobile" src="Icones/Capture_Mobile.png"></a>
 &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;<a href="https://www.latranchesurmer.fr/" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 40px; height: 36px;" alt="La Tranche" src="Icones/La_Tranche.JPG" _base_href="http://meteofloralies.free.fr/"></a>&nbsp;&nbsp;
-&nbsp; &nbsp; &nbsp; <a href="https://pv.viewsurf.com/2010/La-Tranche-sur-mer-Plage-de-la-Terriere?i=NzM2NDp1bmRlZmluZWQ" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 36px; height: 37px;" alt="Terrière" src="/Icones/Webcam.png" _base_href="http://meteofloralies.free.fr/"></a><br>
-<div style="text-align: center;">&nbsp;Mise en page pour téléphone mobile<br>
+&nbsp; &nbsp; &nbsp; <a href="https://pv.viewsurf.com/2010/La-Tranche-sur-mer-Plage-de-la-Terriere?i=NzM2NDp1bmRlZmluZWQ" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 36px; height: 37px;" alt="TerriÃ¨re" src="/Icones/Webcam.png" _base_href="http://meteofloralies.free.fr/"></a><br>
+<div style="text-align: center;">&nbsp;Mise en page pour tÃ©lÃ©phone mobile<br>
 <br>
 
 </div>
@@ -141,7 +141,7 @@ direct&nbsp; &nbsp;<small><small>&nbsp;</small></small>&nbsp; &nbsp; Mobile &nbs
 
 <div style="text-align: center;">Album Photos<br>
 </div>
-<div style="text-align: center;">&nbsp;&nbsp;&nbsp;&nbsp; <a href="Album_Photos.html" _base_href="http://meteofloralies.free.fr/"><img style="border: 0px solid ; width: 66px; height: 35px;" alt="Photos" src="/Icones/Photo.JPG" _base_href="http://meteofloralies.free.fr/"></a>&nbsp; &nbsp;&nbsp;&nbsp;
+<div style="text-align: center;">&nbsp;&nbsp;&nbsp;&nbsp; <a href="Album_Photos.html" _base_href="https://meteofloralies.github.io"><img style="border: 0px solid ; width: 66px; height: 35px;" alt="Photos" src="/Icones/Photo.JPG" _base_href="http://meteofloralies.free.fr/"></a>&nbsp; &nbsp;&nbsp;&nbsp;
 </div>
 
 <div style="text-align: center;"><span style="background-color: rgb(0, 51, 153); color: rgb(0, 51, 153);"></span>
@@ -152,17 +152,17 @@ mer</span> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
 &nbsp;
 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
 &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; <span style="background-color: rgb(0, 51, 153); color: rgb(0, 51, 153);">Fin
-de journée</span></div>
+de journÃ©e</span></div>
 
 </div>
 
-<div style="text-align: center;">&nbsp;&nbsp; <img src="Photos/Embarcadere.JPG" alt="Embarcadère" style="width: 265px; height: 150px;" _base_href="http://meteofloralies.free.fr/">&nbsp;&nbsp;
-<img alt="Dune" src="/Photos/Bute.jpg" style="width: 265px; height: 150px;" _base_href="http://meteofloralies.free.fr/">&nbsp;&nbsp; <img style="width: 265px; height: 149px;" src="Photos/Canyon.jpg" alt="Canyon" _base_href="http://meteofloralies.free.fr/">&nbsp;&nbsp; <img src="/Photos/Mer.jpg" alt="Mer" style="width: 265px; height: 152px;">&nbsp;&nbsp;  <img style="width: 265px; height: 150px;" alt="Dune au loin" src="/Photos/Dune_au_loin.jpg">&nbsp; &nbsp;
+<div style="text-align: center;">&nbsp;&nbsp; <img src="Photos/Embarcadere.JPG" alt="EmbarcadÃ¨re" style="width: 265px; height: 150px;" _base_href="http://meteofloralies.free.fr/">&nbsp;&nbsp;
+<img alt="Dune" src="/Photos/Bute.jpg" style="width: 265px; height: 150px;" _base_href="https://meteofloralies.github.io">&nbsp;&nbsp; <img style="width: 265px; height: 149px;" src="Photos/Canyon.jpg" alt="Canyon" _base_href="http://meteofloralies.free.fr/">&nbsp;&nbsp; <img src="/Photos/Mer.jpg" alt="Mer" style="width: 265px; height: 152px;">&nbsp;&nbsp;  <img style="width: 265px; height: 150px;" alt="Dune au loin" src="/Photos/Dune_au_loin.jpg">&nbsp; &nbsp;
 </div>
 
 <div style="text-align: center;"><span style="background-color: rgb(0, 51, 153);"><span style="color: rgb(0, 51, 153);">il faut
 monter...monter...monter...</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<span style="color: rgb(0, 51, 153);">...et voilà le résultat</span></span>
+<span style="color: rgb(0, 51, 153);">...et voilÃ  le rÃ©sultat</span></span>
 </div>
 
 <div style="text-align: center;">
@@ -175,11 +175,11 @@ monter...monter...monter...</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbs
 </div>
 </div>
 
-<div style="text-align: center;">Après l'eau de mer....l'eau douce<br>
+<div style="text-align: center;">AprÃ¨s l'eau de mer....l'eau douce<br>
 <br>
 </div>
 
-<div> <video width="350" height="auto" controls="controls" poster="http://meteofloralies.free.fr/Mesanges.jpg"> <source src="Mesanges.mp4" type="video/mp4"> </source>
+<div> <video width="350" height="auto" controls="controls" poster="https://meteofloralies.github.io/Mesanges.jpg"> <source src="Mesanges.mp4" type="video/mp4"> </source>
 </video></div>
 
 <br>
@@ -188,14 +188,14 @@ monter...monter...monter...</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbs
 <br>
 </div>
 
-<div style="text-align: center;"><a href="/2024/Janvier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Janvier" src="Icones/Bouton_Janvier.png"></a>&nbsp;&nbsp; &nbsp;&nbsp;<a href="2024/Fevrier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Février" src="/Icones/Bouton_Fevrier.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Mars/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mars" src="Icones/Bouton_Mars.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Avril/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Avril" src="Icones/Bouton_Avril.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Mai/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mai" src="/Icones/Bouton_Mai.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Juin/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juin" src="Icones/Bouton_Juin.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Juillet/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juillet" src="Icones/Bouton_Juillet.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Aout/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Août" src="/Icones/Bouton_Aout.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Septembre/Pluie.html"><img style="border: 0px solid ; width: 90px; height: 29px;" alt="Septembre" src="Icones/Bouton_Septembre.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Octobre/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Octobre" src="Icones/Bouton_Octobre.png"></a><br>
+<div style="text-align: center;"><a href="/2024/Janvier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Janvier" src="Icones/Bouton_Janvier.png"></a>&nbsp;&nbsp; &nbsp;&nbsp;<a href="2024/Fevrier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="FÃ©vrier" src="/Icones/Bouton_Fevrier.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Mars/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mars" src="Icones/Bouton_Mars.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Avril/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Avril" src="Icones/Bouton_Avril.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Mai/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mai" src="/Icones/Bouton_Mai.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Juin/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juin" src="Icones/Bouton_Juin.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Juillet/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juillet" src="Icones/Bouton_Juillet.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Aout/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="AoÃ»t" src="/Icones/Bouton_Aout.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Septembre/Pluie.html"><img style="border: 0px solid ; width: 90px; height: 29px;" alt="Septembre" src="Icones/Bouton_Septembre.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Octobre/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Octobre" src="Icones/Bouton_Octobre.png"></a><br>
 </div>
 
 <div><br>
 <br>
-Prévisions: cliquez ci-dessous</div>
+PrÃ©visions: cliquez ci-dessous</div>
 
-<div style="text-align: center;"><a href="https://www.meteoetradar.com/meteo/la-tranche-sur-mer/4114427"><img style="border: 0px solid ; width: 109px; height: 63px;" alt="Prévisions" src="/Icones/Pluie_Soleil.png"></a><br>
+<div style="text-align: center;"><a href="https://www.meteoetradar.com/meteo/la-tranche-sur-mer/4114427"><img style="border: 0px solid ; width: 109px; height: 63px;" alt="PrÃ©visions" src="/Icones/Pluie_Soleil.png"></a><br>
 
 </div>
 
@@ -213,22 +213,22 @@ Prévisions: cliquez ci-dessous</div>
 <div style="text-align: left;"><span style="color: rgb(51, 255, 51);"></span><br>
 </div>
 <div style="text-align: center;">
-Statistiques 2018 à 2023<br>
+Statistiques 2018 Ã  2023<br>
 </div>
 <div style="text-align: center;">
-Cliquez sur les flèches ci-dessous
+Cliquez sur les flÃ¨ches ci-dessous
 </div>
-<div style="text-align: center;"><!-- liste déroulante pour les années -->
+<div style="text-align: center;"><!-- liste dÃ©roulante pour les annÃ©es -->
 <select id="annee" name="annee" size="1" onchange="affichePage();">
-<option value="">Choisir une Année.....</option>
+<option value="">Choisir une AnnÃ©e.....</option>
 <option value="2023">2023</option>
 <option value="2022">2022</option>
 <option value="2021">2021</option>
 <option value="2020">2020</option>
 <option value="2019">2019</option>
-<option value="2018">2018 à partir du mois d'août</option>
+<option value="2018">2018 Ã  partir du mois d'aoÃ»t</option>
 </select>
-<!-- liste déroulantes pour les mois -->
+<!-- liste dÃ©roulantes pour les mois -->
 <select id="mois" name="mois" size="1" onchange="affichePage();">
 <option value="">Choisir un mois.....</option>
 <option value="Janvier">Janvier</option>
@@ -244,7 +244,7 @@ Cliquez sur les flèches ci-dessous
 <option value="Novembre">Novembre</option>
 <option value="Decembre">Decembre</option>
 </select>
-<!-- liste déroulante pour les pages -->
+<!-- liste dÃ©roulante pour les pages -->
 <select id="pages" name="pages" size="1" onchange="affichePage();">
 <option value="">Choisir une page.....</option>
 <option value="Pluie">Pluie</option>
@@ -267,7 +267,7 @@ Tranche sur Mer</big></big></big><br>
 &nbsp;&nbsp; &nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;
-&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <a href="https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/la-tranche-sur-mer-1080/juillet-2023"><img style="border: 0px solid ; width: 26px; height: 26px;" alt="Heures des marées" src="Icones/Horloge_Station.png"></a>&nbsp;
+&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <a href="https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/la-tranche-sur-mer-1080/juillet-2023"><img style="border: 0px solid ; width: 26px; height: 26px;" alt="Heures des marÃ©es" src="Icones/Horloge_Station.png"></a>&nbsp;
 &nbsp; &nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;
@@ -275,7 +275,7 @@ Tranche sur Mer</big></big></big><br>
 </div>
 <div style="text-align: center;">
 <div style="text-align: center;">&nbsp;&nbsp; Heures des
-marées<br>
+marÃ©es<br>
 </div>
 
 <br>
@@ -283,9 +283,9 @@ marées<br>
 <div style="text-align: center;"><big><a href="/2018/Pluie_2018.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2018" src="/Icones/Pluie_2018.png"></a>&nbsp;&nbsp;&nbsp;<a href="/2019/Pluie_2019.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2019" src="/Icones/Pluie_2019.png"></a>&nbsp;&nbsp;&nbsp;<a href="2020/Pluie2020.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2020" src="/Icones/Pluie_2020.png"></a>&nbsp;&nbsp;&nbsp;<a href="2021/Pluie2021.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2021" src="Icones/Pluie_2021.png"></a>&nbsp;&nbsp;&nbsp;<a href="2022/Pluie_2022.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2022" src="/Icones/Pluie_2022.png"></a>&nbsp;&nbsp;&nbsp;<a href="/2023/Pluie_2023.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2023" src="Icones/Pluie_2023.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Pluie_2024.html"><img style="border: 0px solid ; width: 100px; height: 100px;" alt="Pluie 2024" src="/Icones/Pluie_2024.png"></a></big><br>
 <br>
 </div>
-<div style="text-align: center;"><big><big><span style="font-weight: bold;">Graphiques de la journée</span></big></big><br>
+<div style="text-align: center;"><big><big><span style="font-weight: bold;">Graphiques de la journÃ©e</span></big></big><br>
 </div>
-<div style="text-align: center;"><img src="2024/Octobre/Graphique_Jours.jpg" alt="Grapique de la jounée" style="width: 819px; height: 1212px;"></div>
+<div style="text-align: center;"><img src="2024/Octobre/Graphique_Jours.jpg" alt="Grapique de la jounÃ©e" style="width: 819px; height: 1212px;"></div>
 <div>
 <br>
 </div>
