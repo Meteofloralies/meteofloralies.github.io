46a6cc7068663a4278412d095ec7968cc0245688
Meteofloralies <embrun72@gmail.com>
2024-10-24 10:34:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Index - Météo Floralies</title>
+  
+
+  
+  
+  <meta content="Guy" name="author">
+
+  <base href="http://meteofloralies.free.fr/">
+  
+  <script type="text/javascript">
+function affichePage()
+{
+// récupération des données : annee / mois / pages
+var annee = document.getElementById('annee').value;
+var mois = document.getElementById('mois').value
+var pages = document.getElementById('pages').value
+
+if( annee != '' && mois != '' && pages != '' )
+{
+var url = '/'+ annee + '/' + mois + '/' + pages +'.html';
+document.location.href = url; // on affiche la page
+}
+}
+  </script>
+  
+  <style>
+* { margin:0; padding:0; }
+body {
+width:100%; height:100%;
+max-width:1450px;
+margin:0 auto;
+text-align:center;
+color:white;
+background-color:rgb(0, 51, 153);
+}
+a { color:#dcd296; }
+a:hover { color:#33ff33; }
+a:visited { color:#dcd296; }
+  </style>
+  
+  <script>
+// récupération des données : annee / mois / pages
+var annee = document.getElementById('annee').value;
+var mois = document.getElementById('mois').value
+var pages = document.getElementById('pages').value
+
+if( annee != '' && mois != '' && pages != '' )
+{
+var url = '/'+ annee + '/' + mois + '/' + pages +'.html';
+document.location.href = url; // on affiche la page
+}
+  </script>
+  
+  <style>
+* { margin:0; padding:0; }
+body {
+width:100%; height:100%;
+max-width:1450px;
+margin:0 auto;
+text-align:center;
+color:white;
+background-color:rgb(0, 51, 153);
+}
+/* ------------------ */
+  </style>
+  
+  <script type="text/javascript" id="__gaOptOutExtension">window["_gaUserPrefs"] = { ioo : function() { return true; } }</script>
+  
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" _base_href="http://meteofloralies.free.fr/">
+
+  
+  <style _base_href="http://meteofloralies.free.fr/">
+#widget_9d5499367e786dbed2626a433dae589b {margin: auto;}
+  </style>
+  
+  <link href="cligne2.css" rel="stylesheet" type="text/css"></head><body style="color: white; background-color: rgb(0, 51, 153);" alink="#000088" link="white" vlink="#ff0000">
+<div style="text-align: center;"><big><big style="font-weight: bold; color: rgb(255, 255, 51);"><span style="color: red;">M</span><span style="color: rgb(102, 255, 255);">é</span>t<span style="color: rgb(255, 153, 255);">é</span><span style="color: red;">o</span>f<span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(51, 255, 51);">o</span><span style="color: red;">r<span style="color: white;">a</span></span><span style="color: rgb(102, 255, 255);">l</span><span style="color: rgb(255, 153, 255);">i</span><span style="color: white;">e</span><span style="color: red;">s</span></big></big></div>
+
+<p class="cligne2" style="text-align: center;"><span style="color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La
+mise à jour se fera le plus souvent possible<small>
+</small></span></p>
+
+
+
+<div> <video width="350" height="auto" controls="controls" poster="http://meteofloralies.free.fr/Mesanges.jpg"> <source src="Mesanges.mp4" type="video/mp4"> </source>
+</video></div><br>
+
+
+
+
+
+
+
+<div style="text-align: left;"><a href="/2024/Janvier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Janvier" src="Icones/Bouton_Janvier.png"></a>&nbsp;&nbsp; &nbsp;&nbsp;<a href="2024/Fevrier/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Février" src="/Icones/Bouton_Fevrier.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Mars/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mars" src="Icones/Bouton_Mars.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Avril/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Avril" src="Icones/Bouton_Avril.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Mai/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Mai" src="/Icones/Bouton_Mai.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Juin/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juin" src="Icones/Bouton_Juin.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Juillet/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Juillet" src="Icones/Bouton_Juillet.png"></a>&nbsp;&nbsp;&nbsp; <a href="2024/Aout/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Août" src="/Icones/Bouton_Aout.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Septembre/Pluie.html"><img style="border: 0px solid ; width: 90px; height: 29px;" alt="Septembre" src="Icones/Bouton_Septembre.png"></a>&nbsp;&nbsp;&nbsp; <a href="/2024/Octobre/Pluie.html"><img style="border: 0px solid ; width: 71px; height: 29px;" alt="Octobre" src="Icones/Bouton_Octobre.png"></a><br>
+<div>
+<div style="text-align: center;">&nbsp;  <big>"<span style="color: red;">L</span><span style="color: rgb(51, 204, 255);">e</span><span style="color: yellow;">s</span>
+<span style="color: rgb(51, 204, 255);">F</span>l<span style="color: rgb(255, 102, 0);">o</span><span style="color: yellow;">r</span><span style="color: red;">a</span><span style="color: rgb(204, 51, 204);">l</span>i<span style="color: rgb(51, 204, 255);">e</span><span style="color: red;">s</span>"</big><br>
+</div>
+<div style="text-align: center;"><big><big style="color: rgb(102, 255, 255);"><big>La
+Tranche sur Mer</big></big></big><br>
+
+
+</div>
+<div style="text-align: left;"><br>
+<div style="text-align: center;"><img style="width: 1149px; height: 627px;" alt="Console" src="2024/Octobre/Console.jpg"><big>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </big><br>
+</div>
+</div>
+
+<div style="text-align: left;">
+</div>
+
+<div style="text-align: center;">
+<br>
+</div>
+<div style="text-align: center;"><big><big><span style="font-weight: bold;">Graphiques de la journée</span></big></big><br>
+</div>
+<div style="text-align: center;"><img src="2024/Octobre/Graphique_Jours.jpg" alt="Grapique de la jounée" style="width: 819px; height: 1212px;"><br>
+<br>
+</div>
+
+<div>
+<div style="text-align: center;"><big><big><span style="font-weight: bold;">Graphiques du mois<br>
+</span></big></big></div>
+</div>
+<div style="text-align: center;">
+<img style="width: 1079px; height: 1090px;" alt="Grapique du mois" src="2024/Octobre/Graphique_Mois.jpg"><br>
+</div>
+<br>
+<div style="text-align: center;">&nbsp;<br>
+</div>
+</div>
+</div>
+<div style="text-align: center;">
+<div style="text-align: left;">
+</div>
+
+
+
+
+
+<div>
+</div>
+
+</div>
+</body></html>
